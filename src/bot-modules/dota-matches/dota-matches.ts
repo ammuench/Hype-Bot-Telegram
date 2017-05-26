7cc5e003c6c6d31338c34be6d10048e6c9b96aa6
Alex Muench <ammuench@gmail.com>
2017-05-26 02:28:18 -0500

    Adjsut formatting of live dota2 matches block

--- src/bot-modules/dota-matches/dota-matches.ts
+++ src/bot-modules/dota-matches/dota-matches.ts
@@ -13,11 +13,8 @@ export class DotaMatches {
     this.MyTelegramBot.onText(/\/dotalive/i, (msg: any, match: any): void => {
       this.MyTelegramBot.sendMessage(msg.chat.id, "Fetching Live Dota 2 matches...!");
       this.liveGames()
-        .then((matchStrings) => {
-          for (let i = 0, len = matchStrings.length; i < len; i++) {
-            const hideNotification = (i > 0);
-            this.MyTelegramBot.sendMessage(msg.chat.id, matchStrings[i], { parse_mode: "HTML", disable_web_page_preview: true, disable_notification: hideNotification });
-          }
+        .then((matchString) => {
+          this.MyTelegramBot.sendMessage(msg.chat.id, matchString, { parse_mode: "HTML", disable_web_page_preview: true});
         });
     })
   }
@@ -26,24 +23,23 @@ export class DotaMatches {
     return new Promise((resolve: any, reject: any) => {
       try {
         GosuAPI.fetchMatchUrls("dota2", null, (err, urls) => {
-          const matchArray = ["<b>Current Live Dota 2 Matches</b>"];
+          let matchesString = "<b>Current Live Dota 2 Matches</b>\n==============================\n";
           GosuAPI.parseMatches(urls, (err, matches) => {
             if (!err) {
               for (let match of matches) {
                 if (match.status === "Live") {
-                  matchArray.push(`<b>${match.home.name}</b> vs <b>${match.away.name}</b> -- ${match.rounds} (<a href="${match.url}">Link</a>)`);
+                  matchesString += `\n<b>${match.home.name}</b> vs <b>${match.away.name}</b>\n${match.rounds} (<a href="${match.url}">Link</a>)`;
                 }
               }
-              console.log(matchArray);
-              resolve(matchArray);
+              resolve(matchesString);
             } else {
-              const error = ["<i>Error fetching match URLs from service</i>"];
+              const error = "<i>Error fetching match URLs from service</i>";
               reject(error);
             }
           })
         });
       } catch (e) {
-        const error = ["<i>Error fetching match URLs from service</i>"];
+        const error = "<i>Error fetching match URLs from service</i>";
         reject(error);
       }
     })
