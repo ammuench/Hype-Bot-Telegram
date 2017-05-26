cc90830b040de5cd9191331034de0df243cf7aa0
Carney 3 <claunch3@gmail.com>
2017-05-25 19:44:27 -0500

    Updates to HBT replies - Added new const array of replies to pull from. - hypeReply() now returns a random message from the array.

--- src/hype.ts
+++ src/hype.ts
@@ -1,5 +1,20 @@
+const hypeReplies: string[] = [
+  "HYPE",
+  "HYYYYYYPE",
+  "LET ME CHECK MY WATCH... IT'S HYPE TIME",
+  "FUCKING HYPE",
+  "GET HYPE",
+  "WHEN YOU STARE INTO THE HYPE, THE HYPE STARES BACK",
+  "I AM SO HYPE",
+  "LET THERE BE HYPE",
+  "THERE CAN ONLY BE HYPE",
+  "HYPE HYPE HYPE HYPE HYPE",
+  "TURN UP THE HYPE",
+  "ALL THAT REMAINS IS HYPE",
+  "I AM THE HYPE"
+];
 export class HYPE {
     public hypeReply(): string {
-        return "HYPE";
+        return hypeReplies[Math.floor(Math.random()*hypeReplies.length)];
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,13 @@ import { HYPE } from "./hype";
 const MyTelegramBot = new TelegramBot(token, { polling: true });
 
 
-MyTelegramBot.onText(/hype/i, (msg, match) => {
+MyTelegramBot.onText(/hype/i, (msg: any, match: any): void => {
   const hype = new HYPE();
   MyTelegramBot.sendMessage(msg.chat.id, hype.hypeReply());
 });
 
 // Test Echo Commands
-MyTelegramBot.onText(/\/echo (.+)/, (msg, match) => {
+MyTelegramBot.onText(/\/echo (.+)/, (msg: any, match: any): void => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
