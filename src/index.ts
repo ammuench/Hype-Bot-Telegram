cc5d2a522eebc0f10f62d0d9128e1fee008c4340
Alex Muench <ammuench@gmail.com>
2017-05-26 02:21:51 -0500

    Implement live dota2 match command

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,16 @@ const token = "362547656:AAGAl4o2TvPiXEg1HN4XN5C-zf10xdeuRNk";
 
 import TelegramBot = require("node-telegram-bot-api");
 import { HypeResponses } from "./bot-modules/hype-responses/hype-responses";
+import { DotaMatches } from "./bot-modules/dota-matches/dota-matches";
 
 const MyTelegramBot = new TelegramBot(token, { polling: true });
 
 new HypeResponses(MyTelegramBot);
+new DotaMatches(MyTelegramBot);
 
 // Test Echo Commands
 MyTelegramBot.onText(/\/echo (.+)/, (msg: any, match: any): void => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
   const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
   MyTelegramBot.sendMessage(chatId, resp);
 });
